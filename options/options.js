2015d0f495a2f4251445e801e05b0d2da1fe38b8
Crowdbotics <engineering@crowdbotics.com>
2023-07-18 05:42:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "newdireflow-dev-102472",
+  url: "https://newdireflow-dev-102472.botics.co",
+  api: "https://newdireflow-dev-102472.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
